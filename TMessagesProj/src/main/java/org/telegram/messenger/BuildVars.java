3dbc2e1da1dc4c92ebbc05ab34b41e0e6ec4ec3d
Keeperorowner <1832713361@qq.com>
2024-12-16 18:24:44 +0800

    更新 BuildVars.java

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -28,7 +28,7 @@ public class BuildVars {
     public static final boolean DEBUG_VERSION = false;
     public static boolean DEBUG_PRIVATE_VERSION = false;
     public static boolean CHECK_UPDATES = true;
-    public static boolean LOGS_ENABLED = true;
+    public static boolean LOGS_ENABLED = false;
     public static boolean USE_CLOUD_STRINGS = true;
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static final int BUILD_VERSION = 5511;
